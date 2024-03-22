e532e79da02f7f4d27ef93fa5e99476932be3831
Linus Scott <linsco@edu.umea.se>
2024-03-22 10:21:41 +0100

    first comit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+	import "../app.postcss"
+	import {AppShell} from "@skeletonlabs/skeleton";
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<div class="topBar">
+			<nav class="navbar">
+				<div class="logo">
+					<a class="header" href="/">Linus Scott</a>
+				</div>
+				<div class="menuItems">
+					<div>
+						<a class="barItem" href="/test">test</a>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<hr>
+	</svelte:fragment>
+
+
+	<slot />
+</AppShell>
+
+<style>
+	.header{
+		margin: 15px;
+		text-align: left;
+		font-size: xx-large;
+		font-weight: bold;
+	}
+	.navbar{
+		display: grid;
+		grid-template-columns: [logo-start] 1fr [logo-end] 1fr [menu-start] 1fr [menu-end];
+	}
+	.menuItems{
+		grid-column: menu-start / menu-end;
+	}
+</style>
